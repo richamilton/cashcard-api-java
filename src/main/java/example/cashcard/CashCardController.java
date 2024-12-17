07649c89ee47d5b4a919e6ea7e5d32554a565fb5
richamilton <145603465+richamilton@users.noreply.github.com>
2024-12-17 12:11:06 -0500

    Feature/get controller (#3)
    * add cash card json and tests
    
    * add cash card get controller
    
    * add spring data implementation
    
    ---------
    
    Co-authored-by: Richard Hamilton <r.hamilton@mathstronauts.ca>

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,31 @@
+package example.cashcard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cashcards")
+class CashCardController {
+
+    private final CashCardRepository cashCardRepository;
+
+    CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<CashCard> findById(@PathVariable Long id) {
+        Optional<CashCard> cashCard = cashCardRepository.findById(id);
+        if (cashCard.isPresent()) {
+            return ResponseEntity.ok(cashCard.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}

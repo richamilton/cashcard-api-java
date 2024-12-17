3d89dbe53fc222e3519f50f4eb09613a43758ae2
Richard Hamilton <r.hamilton@mathstronauts.ca>
2024-12-17 12:04:05 -0500

    add spring data implementation

--- src/main/java/example/cashcard/CashCard.java
+++ src/main/java/example/cashcard/CashCard.java
@@ -1,4 +1,5 @@
 package example.cashcard;
+import org.springframework.data.annotation.Id;
 
-public record CashCard(Long id, Double amount) {
+public record CashCard(@Id Long id, Double amount) {
 }
--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,19 +1,29 @@
 package example.cashcard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
+
+    private final CashCardRepository cashCardRepository;
+
+    CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
     @GetMapping("/{id}")
     private ResponseEntity<CashCard> findById(@PathVariable Long id) {
-        if (99L == id){
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
+        Optional<CashCard> cashCard = cashCardRepository.findById(id);
+        if (cashCard.isPresent()) {
+            return ResponseEntity.ok(cashCard.get());
         } else {
             return ResponseEntity.notFound().build();
         }
